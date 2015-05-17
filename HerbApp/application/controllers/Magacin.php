d674d744cefabab22bc5b7204837f9d4de0239e2
Jelena Arsenijevic <jelenarsnjvc@gmail.com>
2015-05-17 21:10:45 +0200

    kontroler-search problem!

--- HerbApp/application/controllers/Magacin.php
+++ HerbApp/application/controllers/Magacin.php
@@ -2,8 +2,58 @@
 
 class Magacin extends CI_Controller{
  
-    public function listaMagacin(){
-        $this->template->load('magacinTemplate', 'magacin/magacinPregled', null);
+ 
+ //lista proizvoda i sirovina u magacinu
+    public function kompletanPregledMagacin(){
+		$proizvodi=$this->proizvodModel->getAll();
+		$sirovine=$this->sirovinaModel->getAll();
+		$data['magacin'][1]=$proizvodi;
+		$data['magacin'][2]=$sirovine;
+        $this->template->load('magacinTemplate', 'magacin/KompletnoStanje', $data);
     }
+	
+	//POCETNA ZA MAGACIN
+	//lista sirovina u magacinu 
+	public function listaMagacin(){
+		$sirovine=$this->sirovinaModel->getAll();
+		$data['sirovina']=$sirovine;
+		$data['pretraga']='';
+		$this->template->load('magacinTemplate','magacin/magacinPregled',$data);
+		
+	}
+	
+	//azuriranje sirovine
+	 public function azurirajSirovinu($idSirovina){
+		$sirovina = $this->sirovinaModel->getById($idSirovina);
+		$data['sirovina'] =  $sirovina;
+        if($sirovina != null){
+        $this->template->load('magacinTemplate', 'magacin/azurirajSirovinu', $data);
+	  }     
+	 }
+	
+	//arhiva-dodatna funkcionalnost
+	public function PregledArhive(){
+	
+	$this->template->load('magacinTemplate', 'magacin/pregledArhive');
+		//todo
+	}
+	
+	//kreiranje sirovine
+	public function kreirajSirovinu(){
+		
+	$this->template->load('magacinTemplate', 'magacin/kreirajSirovinu');
+	}
+	//azuriranje stanja u magacinu
+	public function azurirajStanje(){
+	$this->template->load('magacinTemplate','magacin/azuriranjeStanja');	
+		
+	}
+	
+	//PROBLEM!!
+	public function pretraga($name){
+		$data['name']=$name;
+		//tbd proslednjivanje odgovarajuce sirovine itd.
+		echo $this->load->view('search_view',$data,true);
+	}
+
 }
-?>
