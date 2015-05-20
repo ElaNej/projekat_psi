8f069ccab8819231dd67219f65034d914a3036c3
Jelena Arsenijevic <jelenarsnjvc@gmail.com>
2015-05-21 00:54:38 +0200

    ivanove f.je za c/u sirovina

--- HerbApp/application/views/magacin/azurirajSirovinu.php
+++ HerbApp/application/views/magacin/azurirajSirovinu.php
@@ -1,2 +1,59 @@
-<h1>TODO<h1>
-<?php echo $sirovina->naziv; ?>
+<title>Ažuriranje sirovine</title>
+  
+            <?php echo form_open('magacin/updateSirovina/'.$sirovina->idSirovine.'/'.$sirovina->magacinRez); ?>
+
+            <table>
+			<tr>
+			<td>
+            <label for="naziv">Naziv:</label></td><td>
+            <?php
+            /*$naziv = array(
+                'name' => 'naziv',
+                'id' => 'naziv',
+                'value' => $sirovina->naziv,
+            );*/
+            echo form_input('naziv', $sirovina->naziv);
+            ?></td>
+             </tr>        
+            
+           <tr>
+		   <td>
+            <label for="serBr">Serijski broj:</label></td><td>
+            <?php
+            echo form_input('serBr', $sirovina->serBr);
+            ?></td>
+            </tr>
+            
+            <tr><td>
+            <label for="vremePristiz">Vreme pristizanja:</label></td><td>
+            <?php
+            echo form_input('vremePristiz', $sirovina->vremePristiz);
+            ?></td>
+            </tr>
+            
+            <tr><td>
+            <label for="magacinUk">Ukupno u magacinu:</label></td><td>
+            <?php
+            echo form_input('magacinUk', $sirovina->magacinUk);
+            ?></td>
+            </tr>
+            
+           <tr><td>
+            <label for="magacinRez">Rezervisano u magacinu:</label></td><td>
+            
+            <?php echo form_label($sirovina->magacinRez, 'magacinRez'); ?></td>
+           </tr>
+            
+            <tr><td>
+            <label for="jedinica">Jedinica:</label></td><td>
+            <?php
+            echo form_input('jedinica', $sirovina->jedinica);
+            ?></td>
+           </tr>
+            <tr><td colspan=2 align=center>
+            <input type="submit" value="Save"/>
+            <?php echo form_close(); ?></td>
+			</tr>
+			</table>
+			
+			
--- HerbApp/application/views/magacin/kreirajSirovinu.php
+++ HerbApp/application/views/magacin/kreirajSirovinu.php
@@ -1,1 +1,56 @@
-kreiraj sirovinu
+            <?php echo form_open('magacin/createSirovina/'); ?>
+	<table align=center>
+		<tr>
+            <td>   
+            <label for="naziv">Naziv:</label>
+			</td>
+			<td> 
+            <?php
+            echo form_input('naziv', '');
+            ?>
+			</td> 
+         </tr>            
+            
+         <tr>
+		 <td> 
+            <label for="serBr">Serijski broj:</label>
+			</td> 
+			<td> 
+            <?php
+            echo form_input('serBr', '');
+            ?>
+			</td> 
+         </tr>
+            
+         <tr>
+		 <td> 
+            <label for="vremePristiz">Vreme pristizanja:</label>
+			</td> 
+			<td> 
+            <?php
+            echo form_input('vremePristiz', '');
+            ?>
+			</td> 
+         </tr>
+            
+         <tr>
+		 <td> 
+            <label for="magacinUk">Ukupno u magacinu:</label>  </td> <td> 
+            <?php
+            echo form_input('magacinUk', '');
+            ?>
+			</td> 
+         </tr>
+            
+         <tr>
+		 <td> 
+            <label for="jedinica">Jedinica:</label></td> <td> 
+            <?php
+            echo form_input('jedinica', '');
+            ?></td> 
+         </tr>
+          <tr> <td colspan=2 align=center>  
+            <input type="submit" value="Save"/>
+            <?php echo form_close(); ?> </td>
+		</tr>
+	</table>
