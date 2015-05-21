9bdd9b367c43f83615a44e7b54b066a65674f0cf
dzambi <dzambi@outlook.com>
2015-05-21 22:19:14 +0200

    Admin template

--- HerbApp/application/views/templates/myTemplate.php
+++ HerbApp/application/views/templates/myTemplate.php
@@ -4,6 +4,29 @@
         <title>
             This is HerbApp login page
         </title>
+        <script>
+            function validacija() {
+
+                var x = document.getElementById("korisnickoIme").value;
+                if (x == null || x == "") {
+                    alert("Korisnicko ime nije uneto");
+                    return false;
+                }
+                x = document.getElementById("lozinka").value;
+                if (x == null || x == "") {
+                    alert("Lozinka nije uneta");
+                    return false;
+                }
+                var e = document.getElementById("kategorija");
+                x = e.selectedIndex;
+                if (x == -1){
+                    alert("Kategorija nije izabrana");
+                    return false;
+                }
+
+
+            }
+        </script>
     </head>
     
     <body>
