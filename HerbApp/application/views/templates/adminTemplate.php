3895b9aa51a6e98024cbbcc25081149c75ed54d2
dzambi <dzambi@outlook.com>
2015-05-22 00:45:01 +0200

    Dodat css za admin panel

--- HerbApp/application/views/templates/adminTemplate.php
+++ HerbApp/application/views/templates/adminTemplate.php
@@ -1,6 +1,17 @@
 <html>
    
     <head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/nav.css'); ?>" >
+    
+    
+    
         <title>
            HerbApp Admin
         </title>
@@ -23,7 +34,7 @@
                             $("#searchDiv").append("<th>Ime</th><th>Prezime</th><th>Kategorija</th><th></th>");
                             $.each(res, function(index, val) {
                                 var aStr = '<?php echo base_url()?>index.php/Admin/azurirajKorisnikaPrikaz/' + val.idKor;
-                                $("#searchDiv").append("<tr><td>" + val.ime + "</td><td>" + val.prezime + "</td><td>" + val.kategorija + "</td><td><a href=" + aStr + "><input type='submit' value='Azuriraj'></td></a></tr>");
+                                $("#searchDiv").append("<tr><td>" + val.ime + "</td><td>" + val.prezime + "</td><td>" + val.kategorija + "</td><td><a href=" + aStr + "><input type='submit' value='Azuriraj' class='btn btn-default'></td></a></tr>");
                                
                             });
                          
@@ -48,7 +59,7 @@
                             $("#searchDiv").append("<th>Ime</th><th>Prezime</th><th>Kategorija</th><th></th>");
                             $.each(res, function(index, val) {
                                 var aStr = '<?php echo base_url()?>index.php/Admin/azurirajKorisnikaPrikaz/' + val.idKor;
-                                $("#searchDiv").append("<tr><td>" + val.ime + "</td><td>" + val.prezime + "</td><td>" + val.kategorija + "</td><td><a href=" + aStr + "><input type='submit' value='Azuriraj'></td></a></tr>");
+                                $("#searchDiv").append("<tr><td>" + val.ime + "</td><td>" + val.prezime + "</td><td>" + val.kategorija + "</td><td><a href=" + aStr + "><input type='submit' value='Azuriraj' class='btn btn-default'></td></a></tr>");
                                
                             });
                          
@@ -89,16 +100,60 @@
         </script>
     </head>
     
-    <body>
-     
-        <h2>Admin Template</h2>
-         
-        <div class="wrapper">
-             
-            <?php echo $body; ?>
-             
+     <body style="height:100%;">
+ 
+ 
+ <div class="container-fluid">   
+ 
+ 
+ 
+ <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+         <a class="navbar-brand" rel="home" href="<?php echo site_url('Admin/home') ?>" title="pocetna">
+        <img style="max-width:120px; margin-top: -15px;"
+             src="<?php echo base_url('assets/img/logo.png'); ?>">
+    </a>
         </div>
-         
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li ><a href="<?php echo site_url('Admin/korisnici'); ?>">Zaposleni</a></li>
+            <li><a href="<?php echo site_url('Admin/home'); ?>">Proizvodi</a></li>
+            <li><a href="<?php echo site_url('Admin/home'); ?>">Sirovine</a></li>
+            <li><a href="<?php echo site_url('Admin/home'); ?>">Nabavka</a></li>
+            
+          </ul>
+
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+     
+        <div class="row" style="padding-top:5%;">
+						<div class="col-md-2 column"></div>
+						<div class="col-md-8 column"> <?php echo $body; ?></div>
+						<div class="col-md-2 column"></div>
+					</div>
+
+     
+           
+       </div>
+	    
+	 
+             
     </body>
-    
+	 <footer class="footer" style="position:absolute;bottom:0;width: 100%;height: 60px;background-color: #f5f5f5;">
+      <div class="container">
+        <p class="text-muted">Tim RĐA © 2015</p>
+      </div>
+    </footer>
+	<script>
+
+</script>
 </html>
+
