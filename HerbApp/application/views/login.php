74774f04d92c70fb4740cc546cbf9c9bebe53711
Jelena Arsenijevic <jelenarsnjvc@gmail.com>
2015-05-22 15:33:32 +0200

    login stranica

--- HerbApp/application/views/login.php
+++ HerbApp/application/views/login.php
@@ -4,21 +4,34 @@
  * u template-u koji prosiruje ova stranica, a sama stranica
  * je samo sredisnji sadrzaj.
  */
-?>
-        <?php echo form_open('login/isLoginOK'); ?>
-        <p>
-            <label for="username">Korisnicko ime:</label>
-            <input type="text" name ="username" id="username">
-        </p>
-        <p>
-            <label for="password">Lozinka:</label>
-            <input type="password" name ="password" id="password">
-        </p>
-        
-        <p>
-            <input type="submit" value="Login"/>
-        </p>
-        
-        <?php echo form_close(); ?>
 
-        <a href="<?php echo site_url('login/registracija') ?>">Registruj se</a>
+?>
+
+
+
+
+<div class="row" style="padding-top:5%;">
+		<div class="col-md-4 column"></div>
+		<div class="col-md-4 column"> 
+
+						<div class="form-group">
+						<?php echo form_open('login/isLoginOK'); ?>
+        
+						<label for="username">Korisnicko ime:</label>
+						<input type="text" name ="username" id="username" class="form-control" placeholder="Unesite korisnicko ime">
+        
+        
+						<label for="password">Lozinka:</label>
+						<input type="password" name ="password" id="password" class="form-control" placeholder="Unesite sifru">
+						<br/>
+						<input type="submit" value="Login" class="btn btn-default"/>
+        
+						<?php echo form_close(); ?>
+						<br>
+						<a href="<?php echo site_url('login/registracija') ?>">Registruj se</a>
+						</div>
+		
+		
+		</div>
+		<div class="col-md-4 column"></div>
+</div>
